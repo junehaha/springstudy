e57102723041e718b0a0bd558198cea7856995b8
Juneha Hwang <skdiwnsgk99@gmail.com>
2023-12-09 00:14:45 +0900

    12월9일 끝

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -4,10 +4,17 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
+        // AppConfig appConfig = new AppConfig();
+      //  MemberService memberService = appConfig.memberService(); // DI
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class); // 어노테이션 기반 컨피그 Appconfig 클래스를 기반으로 객체생성/관리
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
         Member member1 = new Member(1L,"member1", Grade.VIP);
         memberService.join(member1);
 
--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -2,13 +2,24 @@ package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPollicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService{
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
+
+    // private final DiscountPolicy discountPolicy = new FixDiscountPolicy(); 구체 클래스까지 의존하므로 변경해야함. DIP OCP 위반
+    /// private final DiscountPolicy discountPolicy = new RateDiscountPollicy();
+
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
+
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member = memberRepository.findById(memberId);
