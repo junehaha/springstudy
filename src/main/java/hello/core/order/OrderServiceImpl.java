5f347a4659447f849873a6d4475690383944fb11
Juneha Hwang <skdiwnsgk99@gmail.com>
2023-12-13 23:17:39 +0900

    커밋

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -6,7 +6,10 @@ import hello.core.discount.RateDiscountPollicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class OrderServiceImpl implements OrderService{
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
@@ -14,7 +17,7 @@ public class OrderServiceImpl implements OrderService{
     // private final DiscountPolicy discountPolicy = new FixDiscountPolicy(); 구체 클래스까지 의존하므로 변경해야함. DIP OCP 위반
     /// private final DiscountPolicy discountPolicy = new RateDiscountPollicy();
 
-
+    @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
